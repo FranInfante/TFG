87ea412189df4563570a59f1bdba397a9d2bfe03
Fran Infante <infante.portero@gmail.com>
2023-11-22 19:27:24 +0100

    index and logo update along with its style

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <nav class="top-menu">
+            <a href="index.php"><img src="img/BolasLogo.png" alt="Your Logo" class="logo"></a>
+            <ul>
+                <li><a href="/ice-creams">Ice Creams</a></li>
+                <li><a href="/bakery">Bakery</a></li>
+                <li><a href="/gallery">Gallery</a></li>
+                <li><a href="/promotions">Promotions</a></li>
+                <li><a href="/about">About us</a></li>
+                <li><a href="/contact">Contact</a></li>
+                <li><a href="/order-online">Order Online</a></li>
+                <li><a href="javascript:void(0);" id="loginBtn">Login</a></li>
+                <a href="javascript:void(0);" id="registerBtn" class="register-button">Register</a>
+            </ul>
+        </nav>
+    </header>
+
+    <!-- The login form popup -->
+    <div id="loginPopup" class="popup">
+        <div class="popup-content">
+            <span class="close" id="closeLogin">&times;</span>
+            <h2>Login</h2>
+            <form>
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required>
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    </div>
+    <div id="registerPopup" class="popup">
+        <div class="popup-content">
+            <span class="close" id="closeRegister">&times;</span>
+            <h2>Register</h2>
+            <form>
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required>
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required>
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required>
+                <!-- Additional fields as needed -->
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    </div>
+
+    <!-- Your other HTML content -->
+
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> Your Ice Cream Shop. All rights reserved.</p>
+    </footer>
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            // Add your JavaScript code here
+            // For example: document.getElementById("loginBtn").addEventListener("click", function () { /* Your code */ });
+        });
+    </script>
+</body>
+
+</html>
